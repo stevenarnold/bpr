7464354a7f920cef36c2addc4e36945e66e57c36
Steven D. Arnold <stevena@arnold-software.com>
2013-09-02 07:49:18 -0600

    First run at some tests.

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,3 +1,7 @@
+def pending
+  true.should == true
+end
+
 describe "Application 'rm-bpr'" do
   before do
     @app = UIApplication.sharedApplication
@@ -5,5 +9,62 @@ describe "Application 'rm-bpr'" do
 
   it "has one window" do
     @app.windows.size.should == 1
+  end
+
+  it "should set rootviewcontroller as BeatCounterViewController" do
+    @app.keyWindow.rootViewController.class.should == BeatCounterViewController
+  end
+end
+
+describe "BeatCounterViewController" do
+  before do
+    @app = UIApplication.sharedApplication
+  end
+
+  it "should have a timer" do
+    pending
+  end
+
+  it "should have a target BPM textbox" do
+    pending
+  end
+
+  it "should have a time to run textbox" do
+    pending
+  end
+
+  it "should have a reset button" do
+    pending
+  end
+
+  it "should have an in/out breathing button" do
+    pending
+  end
+end
+
+describe "Golden Ratio algorithm" do
+  before do
+    @app = UIApplication.sharedApplication
+    @app_delegate = AppDelegate.new
+    @app_delegate.defaults = NSUserDefaults.standardUserDefaults
+    @timer_vc = TimerViewController.new
+    @timer_vc.delegate = @app_delegate
+    @timer_vc.target_bpm = 6.5
+    @timer_vc.time_to_run = 10
+    @timer_vc.act_in_avg = 17
+    @timer_vc.act_out_avg = 17
+    @timer_vc.initialize_sounds
+  end
+
+  it "should change the ratio more toward the Golden Ratio" do
+    pending
+  end
+
+  it "should reduce @act_out_avg and increase @act_in_avg" do
+    pending
+  end
+
+  it "should eventually approximate the Golden Ratio" do
+    pending
   end
 end
